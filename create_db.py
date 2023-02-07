9b92dcf915a3f5bca537f7c7546cda7ffe5dd563
Mateusz Szczepaniuk <matszczepaniuk@gmail.com>
2023-02-07 19:56:40 +0100

    Tables creation code

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 from psycopg2 import connect, OperationalError
 from psycopg2.errors import DuplicateDatabase, DuplicateTable
 
-CREATE_DB = '''CREATE DATABASE workshop''';
+CREATE_DB = '''CREATE DATABASE messages_library''';
 
 CREATE_USERS_TABLE = '''CREATE TABLE users(
     id serial PRIMARY KEY, 
@@ -19,15 +19,44 @@ DB_USER = "postgres"
 DB_PASSWORD = "coderslab"
 DB_HOST = "127.0.0.1"
 
-try:
-    cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-    cnx.autocommit = True
-    cursor = cnx.cursor()
+def create_database():
     try:
-        cursor.execute(CREATE_DB)
-        print("Database created")
-    except DuplicateDatabase as e:
-        print("Database already exists", e)
-    cnx.close()
-except OperationalError as e:
-    print("Connection Error: ", e)
+        cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        try:
+            cursor.execute(CREATE_DB)
+            print("Database created")
+        except DuplicateDatabase as e:
+            print("Database already exists", e)
+        cnx.close()
+    except OperationalError as e:
+        print("Connection Error: ", e)
+
+def create_users_table():
+    try:
+        cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        try:
+            cursor.execute(CREATE_USERS_TABLE)
+            print("Table created")
+        except DuplicateTable as e:
+            print("Such table already exists", e)
+        cnx.close()
+    except OperationalError as e:
+        print("Connection Error: ", e)
+
+def create_messages_table():
+    try:
+        cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        try:
+            cursor.execute(CREATE_MESSAGES_TABLE)
+            print("Table created")
+        except DuplicateTable as e:
+            print("Such table already exists", e)
+        cnx.close()
+    except OperationalError as e:
+        print("Connection Error: ", e)
